c638d0650f43960c12516f79e956f96b851d481e
Mateusz Buda <mateusz@narfsoftware.pl>
2014-05-10 15:27:35 +0200

    init

---
+++ AWK_/MainWindow.xaml.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Reflection;
+using System.IO;
+using AWK;
+using System.Drawing;
+
+namespace AWK_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool playing = false;
+
+        double speed = 1.0;
+        int length = 10;
+        int listSize = 4;
+        int colors = 4;
+
+        int i = 0;
+        int tick = 0;
+        Random r = new Random();
+
+        const int ellipseSize = 36;
+        const int gridColumnWidth = ellipseSize + 2;
+
+        SolidColorBrush[] b = {   System.Windows.Media.Brushes.Red, 
+                                  System.Windows.Media.Brushes.LawnGreen, 
+                                  System.Windows.Media.Brushes.Yellow, 
+                                  System.Windows.Media.Brushes.LightBlue,
+                                  System.Windows.Media.Brushes.Aqua,
+                                  System.Windows.Media.Brushes.HotPink,
+                                  System.Windows.Media.Brushes.Salmon,
+                                  System.Windows.Media.Brushes.DarkMagenta,
+                                  System.Windows.Media.Brushes.Peru,
+                                  System.Windows.Media.Brushes.Tan
+                              };
+
+        Grid seq;
+        Grid ind;
+        Grid list;
+        Canvas img;
+        Button pause;
+        Ellipse[] ellipses;
+        Label[] rands;
+        DispatcherTimer timer = null;
+        BitmapImage arrow;
+        Bitmap image;
+
+        Sequence S;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            pause = this.PauseResume_Button;
+            pause.IsEnabled = false;
+            Next.IsEnabled = false;
+        }
+
+        private void Init_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                length = Convert.ToInt32(this.length_TextBox.Text);
+                listSize = Convert.ToInt32(this.listSize_TextBox.Text);
+                colors = Convert.ToInt32(this.colors_TextBox.Text);
+            }
+            catch (FormatException)
+            {
+                length = 0;
+                listSize = 4;
+                colors = 4;
+            }
+
+            if (colors < listSize || colors > 10)
+            {
+                colors = 4;
+                listSize = 4;
+            }
+
+            init();
+
+            if (timer == null)
+            {
+                timer = new System.Windows.Threading.DispatcherTimer();
+                timer.Tick += new EventHandler(dispatcherTimer_Tick);
+                timer.Interval = new TimeSpan(0, 0, 0, 0, (int)(speed * 1000));
+                timer.Start();
+            }
+
+            pause.IsEnabled = true;
+            pause.Content = "Pause";
+            Next.IsEnabled = true;
+            playing = true;
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (playing)
+            {
+                int h = 0;
+
+                if (tick % 2 == 0 && i < S.seq.GetLength(0))
+                {
+                    S.seq[i] = r.Next(0, listSize);
+                    this.Rand.Content = Convert.ToString(S.seq[i]);
+
+                    ellipses[i].Fill = b[S.list[i, S.seq[i]]];
+                    rands[i].Content = Convert.ToString(S.seq[i]);
+
+                    h = S.isRepetitive(i);
+
+                    i = i - h + 1;
+
+                    S.cnt++;
+                    this.Loop.Content = Convert.ToString(S.cnt);
+                }
+
+                if (tick % 2 != 0 && i < S.seq.GetLength(0))
+                {
+                    ind.Children.Clear();
+                    img = new Canvas()
+                    {
+                        Name = "arrow",
+                        Background = new ImageBrush()
+                        {
+                            //ImageSource = new BitmapImage(new Uri(@"Resources/Down-Arrow-Icon.jpg", UriKind.RelativeOrAbsolute)),
+                            ImageSource = arrow,
+                        }
+                    };
+
+                    this.I.Content = Convert.ToString(i);
+
+                    ind.Children.Add(img);
+                    Grid.SetColumn(img, i);
+
+                    scroll.ScrollToHorizontalOffset((double)(i - (int)(this.ActualWidth / 2 / gridColumnWidth)) * (scroll.ScrollableWidth + this.ActualWidth) / (double)length);
+                    //scroll.ScrollToRightEnd();
+
+                    int j = i;
+                    while (j < ellipses.GetLength(0) && ellipses[j].Fill != System.Windows.Media.Brushes.White)
+                    {
+                        ellipses[j].Fill = System.Windows.Media.Brushes.White;
+                        rands[j].Content = "";
+                        j++;
+                    }
+                }
+
+                tick++;
+            }
+        }
+
+        private void step()
+        {
+            int h = 0;
+
+            if (tick % 2 == 0 && i < S.seq.GetLength(0))
+            {
+                S.seq[i] = r.Next(0, listSize);
+                this.Rand.Content = Convert.ToString(S.seq[i]);
+
+                ellipses[i].Fill = b[S.list[i, S.seq[i]]];
+                rands[i].Content = Convert.ToString(S.seq[i]);
+
+                h = S.isRepetitive(i);
+
+                i = i - h + 1;
+
+                S.cnt++;
+                this.Loop.Content = Convert.ToString(S.cnt);
+            }
+
+            if (tick % 2 != 0 && i < S.seq.GetLength(0))
+            {
+                ind.Children.Clear();
+                img = new Canvas()
+                {
+                    Name = "arrow",
+                    Background = new ImageBrush()
+                    {
+                        //ImageSource = new BitmapImage(new Uri(@"Resources/Down-Arrow-Icon.jpg", UriKind.Relative)),
+                        ImageSource = arrow,
+                    }
+                };
+
+                this.I.Content = Convert.ToString(i);
+
+                ind.Children.Add(img);
+                Grid.SetColumn(img, i);
+
+                int j = i;
+                while (j < ellipses.GetLength(0) && ellipses[j].Fill != System.Windows.Media.Brushes.White)
+                {
+                    ellipses[j].Fill = System.Windows.Media.Brushes.White;
+                    rands[j].Content = "";
+                    j++;
+                }
+            }
+
+            tick++;
+        }
+
+        private void init()
+        {
+            this.i = 0;
+            scroll.ScrollToHome();
+
+            S = new Sequence(length, 0, colors - 1, listSize);
+
+            this.I.Content = Convert.ToString(i);
+            this.Loop.Content = Convert.ToString(S.cnt);
+
+            seq = this.Sequence;
+            ind = this.Index;
+            list = this.Lists;
+
+            seq.ColumnDefinitions.Clear();
+            seq.Children.Clear();
+
+            ind.ColumnDefinitions.Clear();
+            ind.Children.Clear();
+
+            list.ColumnDefinitions.Clear();
+            list.Children.Clear();
+
+            for (int k = 0; k < length; k++)
+            {
+                seq.ColumnDefinitions.Add(new ColumnDefinition
+                {
+                    Width = new GridLength(gridColumnWidth),
+                });
+
+                ind.ColumnDefinitions.Add(new ColumnDefinition
+                {
+                    Width = new GridLength(gridColumnWidth),
+                });
+
+                list.ColumnDefinitions.Add(new ColumnDefinition
+                {
+                    Width = new GridLength(gridColumnWidth),
+                });
+            }
+
+            ellipses = new Ellipse[length];
+            rands = new Label[length];
+
+            for (int k = 0; k < length; k++)
+            {
+                Ellipse e = new Ellipse
+                {
+                    Width = ellipseSize,
+                    Height = ellipseSize,
+                    Fill = System.Windows.Media.Brushes.White,
+                    Stroke = System.Windows.Media.Brushes.Black,
+                };
+
+                Label r = new Label
+                {
+                    Content = "",
+                    VerticalAlignment = System.Windows.VerticalAlignment.Center,
+                    HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                };
+
+                seq.Children.Add(e);
+                seq.Children.Add(r);
+
+                Grid.SetColumn(e, k);
+                Grid.SetColumn(r, k);
+
+                ellipses[k] = e;
+                rands[k] = r;
+
+                list.RowDefinitions.Clear();
+
+                for (int j = 0; j < S.listSize; j++)
+                    list.RowDefinitions.Add(new RowDefinition
+                    {
+                        Height = new GridLength(ellipseSize),
+                    });
+
+                for (int j = 0; j < S.listSize; j++)
+                {
+                    Ellipse f = new Ellipse
+                    {
+                        Width = ellipseSize,
+                        Height = ellipseSize,
+                        Fill = b[S.list[k, j]],
+                        Stroke = System.Windows.Media.Brushes.Black,
+                    };
+
+                    Label l = new Label
+                    {
+                        Content = Convert.ToString(j),
+                        VerticalAlignment = System.Windows.VerticalAlignment.Center,
+                        HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                    };
+
+                    list.Children.Add(f);
+                    list.Children.Add(l);
+                    Grid.SetColumn(f, k);
+                    Grid.SetColumn(l, k);
+                    Grid.SetRow(f, j);
+                    Grid.SetRow(l, j);
+                }
+            }
+
+            if (length != 0)
+            {
+                img = new Canvas()
+                {
+                    Name = "arrow",
+                    Background = new ImageBrush()
+                    {
+                        //ImageSource = new BitmapImage(new Uri(@"Resources/Down-Arrow-Icon.jpg", UriKind.RelativeOrAbsolute)),
+                        //ImageSource = new BitmapImage(new Uri("pack://application:,,,/Resources/Arrow.jpeg", UriKind.RelativeOrAbsolute)),
+                        ImageSource = arrow,
+                    }
+                };
+
+                ind.Children.Add(img);
+                Grid.SetColumn(img, 0);
+            }
+
+            this.Height = ellipseSize * (listSize + 6);
+        }
+
+        private void PauseResume_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (playing)
+            {
+                playing = false;
+                pause.Content = "Resume";
+            }
+
+            else
+            {
+                playing = true;
+                pause.Content = "Pause";
+            }
+        }
+
+        private void step(object sender, RoutedEventArgs e)
+        {
+            if (!playing)
+                step();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            speed = 4 - e.NewValue * 2;
+            if (timer != null)
+            {
+                timer.Interval = new TimeSpan(0, 0, 0, 0, (int)(speed * 1000));
+                System.Console.WriteLine((int)(speed * 1000));
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.Reflection.Assembly myAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+                Stream myStream = myAssembly.GetManifestResourceStream("AWK_.Resources.Arrow.jpeg");
+                image = new Bitmap(myStream);
+
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    image.Save(memory, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    memory.Position = 0;
+                    arrow = new BitmapImage();
+                    arrow.BeginInit();
+                    memory.Seek(0, SeekOrigin.Begin);
+                    arrow.StreamSource = memory;
+                    arrow.CacheOption = BitmapCacheOption.OnLoad;
+                    arrow.EndInit();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error accessing resources!");
+            }
+
+        }
+
+    } // class MainWindow
+} // namespace AWK_
---
+++ AWK_/Sequence.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWK
+{
+    public class Sequence
+    {
+        public int[] seq;       //ciąg wybranych pozycji na liście
+        public int[,] list;    //tablica z listami kolorów
+        public int len;        //długość listy
+        public int listSize;   //rozmiar list z kolorami
+        public int cnt = 0;     //licznik losować w głównej pętli
+
+        public Sequence(int _len, int _listSize = 4)
+        {
+            len = _len;
+            listSize = _listSize < 3 ? 3 : _listSize;
+            seq = new int[len];
+            list = new int[len, listSize];
+
+        }
+
+        //konstruktor tworzy losowe listy
+        //(elementy na liście są w losowej kolejności)
+        //z zadanego przedziału
+        //więc w taki układzie mógłbym nie losować wybieranego koloru z listy,
+        //bo same listy są przemieszane a to, że losuję sprawia, że mam podwójne losowanie
+        public Sequence(int _len, int minVal, int maxVal, int _listSize = 4)
+        {
+            if (maxVal - minVal < listSize - 1)
+            {
+                minVal = 0;
+                maxVal = listSize - 1;
+            }
+
+            len = _len;
+            listSize = (_listSize < 3 || _listSize > 10) ? 4 : _listSize;
+            seq = new int[len];
+            list = new int[len, listSize];
+
+            Random r = new Random();
+
+            for (int i = 0; i < len; i++)
+            {
+                List<int> tmp = new List<int>(listSize);
+                //int tt = minVal;
+                while (tmp.Count < listSize)
+                {
+                    int t = r.Next(minVal, maxVal + 1);
+                    //t = tt;
+                    if (!tmp.Contains(t))
+                        tmp.Add(t);
+                    //tt++;
+                }
+
+                for (int j = 0; j < listSize; j++)
+                    list[i, j] = tmp[j];
+            }
+        }
+
+        //nie ma konstruktora który przyjmowałby gotową tablicę list,
+        //bo komu by się chciało pisać tyle list
+
+
+        //sprawdza czy ciąg ma repetycję na pozycjach < n
+        //zwraca rozmiar repetycji
+        public int isRepetitive(int n)
+        {
+            if (n > len)
+                throw new ArgumentException();
+            //nie jestem pewien czy to dobrze, że rzucam wyjątek i go nigdzie nie łapię...
+
+            int k = n - 1;
+
+            while (k >= 0)
+            {
+                int h = (n - k + 1) / 2;
+
+                if (this.hasRepetition(k, h))
+                    return h;
+
+                k -= 2;
+            }
+
+            return 0;
+        }
+
+        //sprawdza czy jest repetycja o długości h i początku na indeksie k
+        private bool hasRepetition(int k, int h)
+        {
+            for (int i = k; i < k + h; i++)
+                if (list[i, seq[i]] != list[i + h, seq[i + h]])
+                    return false;
+
+            return true;
+        }
+
+        //wypisuje ciąg wraz z listami w formacie [1 (a, b, c, d)] [2 (d, c, b, a)]
+        //gdzie liczba to numer wybranego koloru na liście (numeracja od 0)
+        //czyli w tym przykładzie ciąg będzie taki: {a, c}
+        public void writeSequence()
+        {
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write(string.Format("[{0} ({1}, {2}, {3}", seq[i], list[i, 0], list[i, 1], list[i, 2]));
+
+                for (int j = 3; j < listSize; j++)
+                    Console.Write(string.Format(", {0}", list[i, j]));
+
+                Console.Write(")]");
+                Console.WriteLine();
+            }
+        }
+
+        //wypisuje wybrane elementy (nie koloruje bez repetycji)
+        public void writeColoring()
+        {
+            for (int i = 0; i < len; i++)
+                Console.Write(string.Format("{0}, ", list[i, seq[i]]));
+
+            Console.WriteLine();
+        }
+
+        public void Color()
+        {
+            Random r = new Random();
+            int h = 0;
+
+            int i = 0;
+            while (i < seq.GetLength(0))
+            {
+                seq[i] = r.Next(0, listSize);
+
+                h = isRepetitive(i);
+
+                i = i - h + 1;
+
+                cnt++;
+            }
+        }
+
+    }
+}
